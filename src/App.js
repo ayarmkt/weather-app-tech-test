1397dee82b2684eac5ab95292349b9f23e73192c
ayarmkt <ayat.0906@gmail.com>
2022-06-03 10:33:48 +0200

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import { getAllWeatherData } from './API/getWeatherData';
 import { locationData } from './assets/locationData';
+import WeatherMeasureTabs from './components/WeatherMeasureTab/WeatherMeasureTab';
 
 const App = () => {
   getAllWeatherData(locationData);
+
+  return (
+    <>
+      <h1>Widget Weathermap</h1>
+      <WeatherMeasureTabs />
+    </>
+  );
 };
 
 export default App;
